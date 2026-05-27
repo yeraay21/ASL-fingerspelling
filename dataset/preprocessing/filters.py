filters.py modified to fix computation issues

dataset/preprocessing/filters.py
@@ -2,7 +2,8 @@
 
 import numpy as np
 from skimage.filters import gabor_kernel
-from scipy.ndimage import convolve
+import cv2  
+import time 
 
 def build_gabor_bank(frequencies, orientations):
     bank = []
@@ -16,29 +17,31 @@ def extract_features(images_gray, filters):
     N = len(images_gray)
     F = 2 * len(filters)
     out = np.zeros((N, F), dtype=np.float32)
+    
+    t_start = time.time()
+    
     for i, img in enumerate(images_gray):
+        
+        if i > 0 and i % 1000 == 0:
+            elapsed = time.time() - t_start
+            print(f"   -> Gabor : {i}/{N} images traitées en {elapsed:.2f} s")
+            t_start = time.time() # On reset le chrono
+            
         for j, k in enumerate(filters):
-            resp = convolve(img, k, mode="reflect")
-            out[i, 2*j]     = resp.mean()
+            resp = cv2.filter2D(img, -1, k, borderType=cv2.BORDER_REFLECT)
+            
+            out[i, 2*j] = resp.mean()
             out[i, 2*j + 1] = resp.std()
+            
     return out
 
-#uncomment line below to test
-# if __name__ == "__main__":
-    
-    # simulate our images with values in [0,1] (dummy batch method)
+if __name__ == "__main__":
+    # Bloque de test: solo se ejecuta con `python filters.py`, no al importar
     dummy_images = np.random.rand(5, 64, 64)
-
-    # define filters params
     freqs = [0.05, 0.1, 0.15, 0.2, 0.25]
     orientations = 8
-
-    # we execute both functions
     bank = build_gabor_bank(freqs, orientations)
-    
     features = extract_features(dummy_images, bank)
-
-
     print("test results")
     print(f"is shape (5,80)? Shape  : {features.shape}")
     print(f"NaN or not : {np.isnan(features).any()}")